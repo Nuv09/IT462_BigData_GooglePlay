0d0ff3344cbad6c78dca7882ca187c3d07d28f12
nuv09 <Nouf05522@gmail.com>
2026-03-04 19:05:38 +0300

    Renamed DataPreprocessing to DanaCleaning

--- code/01_DanaCleaning.scala
+++ code/01_DanaCleaning.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
-object DataPreprocessing {
+object DanaCleaning {
 
   def main(args: Array[String]): Unit = {
 
